bd470114adadb8bbd4e5594fee53ca7116550c55
Jack <jnd31@case.edu>
2019-04-19 02:48:13 -0400

    Implemented conditioning on confounders

--- calculate.py
+++ calculate.py
@@ -10,74 +10,116 @@ import itertools
 #load_data1 = arff.loadarff('Phishing.arff')
 #df1 = pd.DataFrame(load_data[0]).astype(int)
 
+'''
+Calculates the P(Y=y|A=a) when P(Y=y|A=a, L_i=l_i, ..., L_n=l_n)
+P(Y=y|A=a) = \sum_L P(Y=y|A=a, L=l, ..., L=l)*P(L=l, ..., L=l)
+           = \sum_L P(Y=y,A=a,L=l,...,L=l)*P(L=l,...,L=l)/P(A=a,L=l,...,L=l)
+                    aycounts[a][y][i]      totals[i]      aycounts[a][sum over y][i]
+@param aValueIndex  the index of aycounts where a = value you're looking for
+@param yValueIndex  the index of aycounts[a] where y = value you're looking for
+@param aycounts     it's complicated but each value [a][y][i] is where you're on the ith combo of L
+                    and it's the sum of where [a][y] are their values
+@param ltotals      same as aycounts but no a and y
+@returns            P(Y=y|A=a)
+'''
+def condProb(aValueIndex, yValueIndex, aycounts, ltotals):
+    # TODO: make sure this works when there are no l to sum over
+    probability = 0
+    for l in range(len(ltotals)):
+        alSum = 0
+        for y in aycounts[aValueIndex]:
+            alSum += aycounts[aValueIndex][y][l]
+        probability += aycounts[aValueIndex][yValueIndex][l] * ltotals[i] / alSum
+    return probability
+
+'''
+Calculates the associational risk difference for the cause A, the effect Y, and the conditions L
+@param data     the data to calculate the risk difference on. Each data[i] holds all the values of a
+                    variable, with data[i][j] referring to element j's value for variable i.
+@param indexOfA the index of the cause's values in the data. data[indexOfA][j] represents element
+                    j's value of A.
+@param indexOfY the index of the effect's values in the data. data[indexOfY][j] represents element
+                    j's value of Y.
+@param indexOfL an array holding the indices of different L's values in the data. Optional.
+@returns        the risk difference for A on Y given conditions L
+'''
 def riskDifference(data, indexOfA, indexOfY, indexOfL=[]):
     AValues = [0, 1] # count in the array when A = value and Y = 1
     YValues = [1]
-    LValues = [ [0,1] for _ in range(len(indexOfL))]
-    ALCombos = [_ for l in range(len(indexOfL)+2) for _ in itertools.combinations([indexOfL,'a'], l)] 
-    counts, totals = count(data, indexOfA, indexOfY, AValues, YValues, ALCombos, LValues, indexOfL)
-    #return counts[1][0]/totals[1] - counts[0][0]/totals[0]
+    LCombos = [_ for l in range(len(indexOfL)+1) for _ in itertools.combinations([indexOfL], l)] 
+    counts, totals = count(data, indexOfA, indexOfY, AValues, YValues, LCombos, indexOfL)
+    return condProb(1, 0, counts, totals) - condProb(0, 0, counts, totals)
 
+'''
+Calculates the associational risk ratio for the cause A, the effect Y, and the conditions L
+@param data     the data to calculate the risk ratio on. Each data[i] holds all the values of a
+                    variable, with data[i][j] referring to element j's value for variable i.
+@param indexOfA the index of the cause's values in the data. data[indexOfA][j] represents element
+                    j's value of A.
+@param indexOfY the index of the effect's values in the data. data[indexOfY][j] represents element
+                    j's value of Y.
+@param indexOfL an array holding the indices of different L's values in the data. Optional.
+@returns        the risk ratio for A on Y given conditions L
+'''
 def riskRatio(data, indexOfA, indexOfY, indexOfL=[]):
     AValues = [0,1]
     YValues = [1]
-    LValues = [ [0,1] for _ in range(len(indexOfL))]
-    ALCombos = [_ for l in range(len(indexOfL)+2) for _ in itertools.combinations([indexOfL,'a'], l)] 
-    counts, totals = count(data, indexOfA, indexOfY, AValues, YValues, ALCombos, LValues, indexOfL)
-    #return (counts[1][0]/totals[1])/(counts[0][0]/totals[0])
+    LCombos = [_ for l in range(len(indexOfL)+1) for _ in itertools.combinations([indexOfL], l)] 
+    counts, totals = count(data, indexOfA, indexOfY, AValues, YValues, LCombos, indexOfL)
+    return condProb(1, 0, counts, totals) / condProb(0, 0, counts, totals)
 
+'''
+Calculates the associational odds ratio for the cause A, the effect Y, and the conditions L
+@param data     the data to calculate the odds ratio on. Each data[i] holds all the values of a
+                    variable, with data[i][j] referring to element j's value for variable i.
+@param indexOfA the index of the cause's values in the data. data[indexOfA][j] represents element
+                    j's value of A.
+@param indexOfY the index of the effect's values in the data. data[indexOfY][j] represents element
+                    j's value of Y.
+@param indexOfL an array holding the indices of different L's values in the data. Optional.
+@returns        the odds ratio for A on Y given conditions L
+'''
 def oddsRatio(data, indexOfA, indexOfY, indexOfL=[]):
     AValues = [0,1]
     YValues = [0,1]
-    LValues = [[0,1] for _ in range(len(indexOfL))]
-    ALCombos = [_ for l in range(len(indexOfL)+2) for _ in itertools.combinations([indexOfL,'a'], l)] 
-    counts, totals = count(data, indexOfA, indexOfY, AValues, YValues, ALCombos, LValues, indexOfL)
-    #return (counts[1][1]/totals[1]/(counts[1][0]/totals[1])/(counts[0][1]/totals[0]/(counts[0][0]/totals[0])))
+    LCombos = [_ for l in range(len(indexOfL)+1) for _ in itertools.combinations([indexOfL], l)]
+    # contains all combinations of the indices of A and L's
+    counts, totals = count(data, indexOfA, indexOfY, AValues, YValues, LCombos, indexOfL)
+    return condProb(1, 1, counts, totals)/condProb(1, 0, counts, totals) / (condProb(0, 1, counts, totals)/condProb(0, 0, counts, totals))
 
-def count(data, indexOfA, indexOfY, AValues, YValues, ALCombos, LValues=[], indexOfL=[]):
+'''
+Counts a combination of all L for when a = AValues and y = YValues. 
+Values of A, Y, and L must be discrete. A and Y can be nonbinary, though L's must be binary.
+@param data         the data to count over
+@param indexOfA     the index of A in the data
+@param indexOfY     the index of Y in the data
+@param AValues      the values of A to count for
+@param YValues      the values of Y to count for
+@param LCombos      permutations of all indices of L
+@param indexOfL     the indices of L in the data
+@return aycounts    the counts over the permutations of L for when A=AValues and Y=YValues
+@return totals      the counts over the permutations of L
+'''
+def count(data, indexOfA, indexOfY, AValues, YValues, LCombos, indexOfL=[]):
     #TODO: give counts and totals more descriptive names
-    totals = np.zeros_like(ALCombos)
-    ycounts = np.zeros_like([ALCombos for _ in range(len(YValues))])
-    for i in range(len(data[0])):
-        for j in range(len(ALCombos)):
-            correctCov = True
-            for l in indexOfL:
-                if l in ALCombos[j]:
-                    correctCov = data[l][i] == 1
+    totals = np.zeros_like(LCombos) # totals[j] holds the total number of items that only contain the items in LCombos[j]
+    aycounts = np.zeros_like([[LCombos for _ in YValues] for _ in AValues]) # same but for each of Y=YValues[k]
+    for i in range(len(data[0])): # for all elements in the data
+        for j in range(len(LCombos)): # for all combos of A and L
+            correctCov = True # start off with correctCov as true
+            for l in indexOfL: # check that all the L's that LCombs call for are present
+                if l in LCombos[j]:
+                    correctCov = (data[l][i] == 1) and correctCov
                 else:
-                    correctCov = data[l][i] == 0
-            if 'a' in ALCombos[j]:
-                correctCov = data[indexOfA][i] == 1
-            else:
-                correctCov = data[indexOfA][i] == 0
-            for k in YValues:
-                yCorrect = data[indexOfY][i] == k
-                if correctCov:
-                    totals[j] += 1
-                    if yCorrect:
-                        ycounts[k][j] += 1
-    return ycounts, totals
-
-if __name__ == '__main__':
-    data = np.array([[0,0,0,0,1,1,1], [0,1,1,1,0,1,1]])
-    #A=0, Y=0 = 1
-    #A=0, Y=1 = 3
-    #A=1, Y=0 = 1
-    #A=1, Y=1 = 2
-    counts, totals = count(data, 0, 1, [0, 1], [0,1])
-    print("counts: A=0|Y=0, A=0|Y=1")
-    print("counts: A=1|Y=0, A=1|Y=1")
-    print(counts)
-    print("totals: A=0, A=1")
-    print(totals)
-    print("Y=1,A=1/A=1 - Y=1,A=0/A=0")
-    print(riskDifference(data, 0, 1))
-    print("a=1,y=1/a=1")
-    print("-----------")
-    print("a=0,y=1/a=0")
-    print(riskRatio(data, 0, 1))
-    print("a1,y1/a1 / a1,y0/a1")
-    print("-------------------")
-    print("a0,y1/a0 / a0,y0/a0")
-    print(oddsRatio(data, 0, 1))
+                    correctCov = (data[l][i] == 0) and correctCov
+                if (not(correctCov)):
+                    break
+            for a in range(len(AValues)):
+                for y in range(len(YValues)):
+                    ayCorrect = (data[indexOfA][i] == AValues[a]) and (data[indexOfY][i] == YValues[y])
+                    if correctCov:
+                        totals[j] += 1
+                        if ayCorrect:
+                            aycounts[a][y][j] += 1
+    return aycounts, totals
 
